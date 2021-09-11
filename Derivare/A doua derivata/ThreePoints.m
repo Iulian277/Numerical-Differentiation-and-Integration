0f61da62844ce1d998daef3327f21fc1e8531caa
Iulian Taiatu <31247431+Iulian277@users.noreply.github.com>
2021-09-11 18:32:00 +0300

    Update ThreePoints.m

--- Derivare/A doua derivata/ThreePoints.m
+++ Derivare/A doua derivata/ThreePoints.m
@@ -1,1 +1,12 @@
-function d2f = ThreePoints(f, x0, h)   % Input:   %       Functia f   %       Punctul (x0) in care trebuie sa aproximez a doua derivata a lui f   % Output:   %        Valoarea aproximata (y0) a derivatei de ordin 2 a lui f in x0      % TODO: d2f = Second Derivative Midpoint Formula   d2f = 0; % De sters     endfunction
+function d2f = ThreePoints(f, x0, h)
+   % Input:
+   %       Functia f 
+   %       Punctul (x0) in care trebuie sa aproximez a doua derivata a lui f
+   % Output:
+   %        Valoarea aproximata (y0) a derivatei de ordin 2 a lui f in x0
+   
+   % TODO: d2f = Second Derivative Midpoint Formula
+   d2f = 0; % De sters
+   
+  
+endfunction
